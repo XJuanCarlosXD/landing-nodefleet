5fc2285bfd7fc2b1bce41849a2a6f34b1a54670a
XJuanCarlosXD <juancarlos192003@gmail.com>
2025-03-20 11:22:31 -0400

    chore: update framer-motion to version 12.5.0 and refactor scripts to use react-app-rewired; add javascript-obfuscator and webpack-obfuscator as devDependencies

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const JavaScriptObfuscator = require('webpack-obfuscator');
+
+module.exports = {
+    // ... resto de su configuración webpack
+    plugins: [
+        // ... otros plugins
+        new JavaScriptObfuscator({
+            rotateStringArray: true,
+            stringArray: true,
+            stringArrayEncoding: ['base64'],
+            stringArrayThreshold: 0.8,
+            identifierNamesGenerator: 'hexadecimal',
+            renameGlobals: false,
+            selfDefending: true,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            debugProtection: true,
+            debugProtectionInterval: true,
+            disableConsoleOutput: true,
+            unicodeEscapeSequence: true
+        })
+    ]
+}; 
